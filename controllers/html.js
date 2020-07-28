d045db1f8644dda66a93313b64d7f2ae3e235299
Sara Webb <saracharlotterose@gmail.com>
2020-07-28 10:27:19 -0700

    Adding Mix page functions

--- controllers/html.js
+++ controllers/html.js
@@ -5,26 +5,57 @@ module.exports = function(app) {
     res.render("home");
   });
   app.get("/mix", (req, res) => {
-    // get all ingrediennts from database
-    db.Ingredient.findAll().then(ingredients => {
-      const alcohols = [];
-      const sweeteners = [];
-      const sours = [];
-      ingredients.forEach(myFunction);
-      function myFunction(i) {
-        const name = i.name[0].toUpperCase() + i.name.substr(1);
-        const value = i.name;
-        if (i.category === "alcohol") {
-          alcohols.push({ name, value });
-        } else if (i.category === "sweetener") {
-          sweeteners.push({ name, value });
-        } else {
-          sours.push({ name, value });
-        }
-      }
+    // get all the ingredients
+    db.Ingredient.findAll()
+      .then(ingredients => {
+        const sours = [];
+        const alcohols = [];
+        const sweeteners = [];
+        const others = [];
+        // then sort ingredients to an array by the category
+        ingredients.forEach(ingredient => {
+          const name =
+            ingredient.name[0].toUpperCase() + ingredient.name.substr(1);
+          const value = ingredient.name;
+          //"alcohol", "sour", "sweetener", "other"
+          if (ingredient.category === "alcohol") {
+            alcohols.push({ name, value });
+          } else if (ingredient.category === "sweetener") {
+            sweeteners.push({ name, value });
+          } else if (ingredient.category === "sour") {
+            sours.push({ name, value });
+          } else {
+            others.push({ name, value });
+          }
+        });
+        // render mis with each of the arrays
+        res.render("mix", { alcohols, sours, sweeteners, others });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).end();
+      });
+  });
 
-      res.render("mix", { alcohols, sweeteners, sours });
-    });
+  app.get("/cocktails", (req, res) => {
+    let cocktailsPromise;
+    if (!req.query.ingredients) {
+      cocktailsPromise = db.Cocktail.findAll({
+        include: db.Ingredient
+      }).then(cocktails => cocktails.map(c => c.toJSON()));
+    } else {
+      cocktailsPromise = db.Cocktail.findAllContainingEveryIngredient(
+        req.query.ingredients.split(",")
+      );
+    }
+    cocktailsPromise
+      .then(cocktails => {
+        res.render("cocktails", { cocktails });
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).end();
+      });
   });
   app.get("/cocktails/:id", (req, res) => {
     res.render("cocktail-views-page");
