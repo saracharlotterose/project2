a1746fc3e3794a5c6a17347e99c8af360bd16f84
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-07-27 18:05:33 -0700

    commit on cocktails-view-data

--- controllers/html.js
+++ controllers/html.js
@@ -30,25 +30,14 @@ module.exports = function(app) {
       });
   });
   app.get("/cocktails/:id", (req, res) => {
-    res.render("cocktail-views-page");
-    // update render response after views is created
-    let cocktailsPromise;
-
-    if (!req.query.id) {
-      cocktailsPromise = db.Cocktail.findOne({
-        include: db.id
-      }).then(cocktails => cocktails.map(c => c.toJSON()));
-    } else {
-      cocktailsPromise = db.Cocktail.findOne(req.query.id);
-    }
-    cocktailsPromise
-      .then(id => {
-        res.render("cocktails-view-page", { id });
-        // update hbs template name after views is created
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).end();
-      });
+    db.Cocktail.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Ingredient]
+    }).then(dbDrink => {
+      // res.json(dbDrink);
+      res.render("cocktails-view-page", { dbDrink });
+    });
   });
 };
